fef9e06ff7d2b0a572f397b500f17941995f1763
Steven Velozo <steven@velozo.com>
2024-07-14 00:03:24 -0700

    Fixed up the docuexample for custom data providers to be correct

--- example_applications/complex_table/Complex-Tabular-CustomDataProvider.js
+++ example_applications/complex_table/Complex-Tabular-CustomDataProvider.js
@@ -11,14 +11,14 @@ class CustomInputHandler extends libPictSectionInputExtension
 	{
 		this.log.trace(`CustomInputHandler.onInputInitializeTabular() for view [${pView.Hash}] called`);
 		//this.log.trace(`The input object is: ${JSON.stringify(pInput)}`);
-		return super.onInputInitialize(pView, pGroup, pInput, pHTMLSelector);
+		return super.onInputInitialize(pView, pGroup, pInput, pValue, pHTMLSelector);
 	}
 
 	onInputInitializeTabular(pView, pGroup, pInput, pValue, pHTMLSelector)
 	{
 		this.log.trace(`CustomInputHandler.onInputInitializeTabular() for view [${pView.Hash}] called`);
 		//this.log.trace(`The input object is: ${JSON.stringify(pInput)}`);
-		return super.onInputInitializeTabular(pView, pGroup, pInput, pHTMLSelector);
+		return super.onInputInitializeTabular(pView, pGroup, pInput, pValue, pHTMLSelector);
 	}
 }
 
