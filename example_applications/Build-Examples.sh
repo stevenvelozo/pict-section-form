db075bed419ad59f303a07405d73330dbeac48b0
Steven Velozo <steven@velozo.com>
2025-01-28 10:11:48 -0800

    Fixed build tools and server for advanced (e.g. distilling) examples to be easy to use

--- example_applications/Build-Examples.sh
+++ example_applications/Build-Examples.sh
@@ -2,45 +2,45 @@
 
 # Building complex_table
 pushd complex_table
-npm run build
+npm install && npm run build
 popd
 
 # Building complex_tuigrid
 pushd complex_tuigrid
-npm run build
+npm install && npm run build
 popd
 
 # Building gradebook
 pushd gradebook
-npm run build
+npm install && npm run build
 popd
 
 # Building manifest_testbed
 pushd manifest_testbed
-npm run build
+npm install && npm run build
 popd
 
 # Building manyfest_editor
 pushd manyfest_editor
-npm run build
+npm install && npm run build
 popd
 
 # Building postcard_example
 pushd postcard_example
-npm run build
+npm install && npm run build
 popd
 
 # Building simple_form
 pushd simple_form
-npm run build
+npm install && npm run build
 popd
 
 # Building simple_table
 pushd simple_table
-npm run build
+npm install && npm run build
 popd
 
 # Building simple_table_from_object
 pushd simple_table_from_object
-npm run build
+npm install && npm run build
 popd
