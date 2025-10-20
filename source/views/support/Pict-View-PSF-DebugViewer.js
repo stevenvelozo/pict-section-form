7a8f2ad6aef3755d971b244759c56aa54f405f81
Douglas <doug.greene@paviasystems.com>
2025-10-20 16:49:09 -0700

    Update Pict-View-PSF-DebugViewer.js
    Sandboxes improved styling on DebugView

--- source/views/support/Pict-View-PSF-DebugViewer.js
+++ source/views/support/Pict-View-PSF-DebugViewer.js
@@ -10,26 +10,163 @@ const defaultViewConfiguration = (
 	RenderOnLoad: false,
 
 	CSS: /*css*/`
-	#Pict-Form-Extensions-Container { position: absolute; left: 75%; top: 0px; width: 25%; background-color: blanchedalmond; margin: 2px; }
-	#PSFDV-SectionList .PSFDV-Section { margin: 2px; padding: 2px; background-color: aliceblue; }
-	#PSFDV-DynamicInputSection .PSFDV-Section { margin: 2px; padding: 2px; background-color: #c8fe44; }
-	.PSFDV-Section-Group, .PSFDV-Section-Descriptors { margin-left: 5px; }
-	.PSFDV-Section-Buttons { font-face: orange; }
-	.PSFDV-Solver-Entry { margin-left: 10px; margin-left: 5px; font-family: "Courier New", "Lucida Console", monospace; white-space: nowrap; overflow: scroll; }
-	.PSFDV-Solver-Result { font-family: "Courier New", "Lucida Console", monospace; background-color: #eefaee; white-space: nowrap; overflow: scroll; }
-	.PSFDV-DataHeader { font-weight: bold; }
-	.PSFDV-DeEmphasize { font-weight: light; font-size: smaller; }
-	.PSFDV-Data { margin-left: 10px; line-height: 0.5; font-size: smaller; }
-	.PSFDV-Label { font-weight: light; min-width: 15%; color: #334481; }
-	.PSFDV-Hidden { display: none; }
+	:root{
+		--PSFDV-gutter-size: 5px;
+		--PSFDV-indentation-size: calc(2 * var(--PSFDV-gutter-size));
+		--PSFDV-Global-background-color: #dcdce5;
+		--PSFDV-Global-text-color: #333333;
+		--PSFDV-Section-background-color: #cacae8;
+		--PSFDV-Section-button-color: #5A52A3;
+		--PSFDV-Section-button-text-color: #D8D7E5;
+		--PSFDV-Section-Data-background-color: #e2e2f0;
+		--PSFDV-Section-DynamicInput-background-color: #a3ccd8;
+		--PSFDV-Section-DynamicInput-button-color: #2b89a4;
+		--PSFDV-Section-DynamicInput-button-text-color: #D8D7E5;
+	}
+	#Pict-Form-Extensions-Container { 
+		position: absolute;
+		left: 50%;
+		top: 0px;
+		width: 50vw;
+		height: 100vh;
+		color: var(--PSFDV-Global-text-color);
+		background-color: var(--PSFDV-Global-background-color);
+		padding: var(--PSFDV-gutter-size);
+		overflow: auto;
+		max-height: 100vh;
+		font-family: Arial, sans-serif;
+		font-size: 14px;
+	}
+	.PSFDV-Global-Header {
+		margin: var(--PSFDV-gutter-size);
+	}
+	.PSFDV-Section-Header {
+		margin: var(--PSFDV-gutter-size);
+	}
+	.PSFDV-Content-Header {
+		margin: var(--PSFDV-gutter-size);
+	}
+	#PSFDV-SectionList .PSFDV-Section { 
+		margin: var(--PSFDV-gutter-size);
+		padding: var(--PSFDV-gutter-size);
+		background-color: var(--PSFDV-Section-background-color);
+	}
+	.PSFDV-Section-Descriptors { 
+		border-left: 5px solid var(--PSFDV-Section-background-color);
+		background-color: #ffffff;
+		padding: var(--PSFDV-gutter-size) 0;
+	}
+	.PSFDV-Section-Group { 
+		border-left: 5px solid var(--PSFDV-Section-button-color);
+		padding: var(--PSFDV-gutter-size);
+		background-color: #efefef;
+	}
+	.PSFDV-Section-Buttons {
+		list-style-type: none;
+		padding: 0;
+		margin: calc(var(--PSFDV-gutter-size) * 2) var(--PSFDV-gutter-size);
+	}
+	.PSFDV-Section-Button { 
+		display: inline;
+		margin-right: calc(var(--PSFDV-gutter-size) * 0.25);
+		text-decoration: none;
+		background-color: var(--PSFDV-Section-button-color);
+		padding: var(--PSFDV-gutter-size);
+		border-radius: var(--PSFDV-gutter-size);
+	}
+	.PSFDV-Section-Button a { 
+		text-decoration: none;
+		color: var(--PSFDV-Section-button-text-color);
+	}
+	.PSFDV-Section-Button a:hover { 
+		text-decoration: underline;
+	}
+	.PSFDV-Solver-Entry { 
+		padding-left: var(--PSFDV-indentation-size);
+		margin-left: var(--PSFDV-gutter-size);
+		font-family: "Courier New", "Lucida Console", monospace;
+		white-space: nowrap;
+		overflow: scroll;
+	}
+	.PSFDV-Solver-Result { 
+		font-family: "Courier New", "Lucida Console", monospace;
+		background-color: #dedede;
+		border: 1px solid #cccccc;
+		font-weight: bold;
+		border-radius: var(--PSFDV-gutter-size);
+		margin-top: var(--PSFDV-gutter-size);
+		display: inline-block;
+		white-space: nowrap;
+		overflow: scroll;
+		padding: var(--PSFDV-gutter-size);
+	}
+	.PSFDV-DataHeader {
+		font-weight: bold;
+		background-color: #f0f0f0;
+		padding: var(--PSFDV-gutter-size);
+	}
+	.PSFDV-Section-ExtraData {
+		margin-top: var(--PSFDV-gutter-size);
+		padding: var(--PSFDV-gutter-size);
+		background-color: var(--PSFDV-Section-Data-background-color);
+	}
+	.PSFDV-Section .PSFDV-DeEmphasize
+	{
+		color: var(--PSFDV-Section-button-color);
+	}
+	.PSFDV-DeEmphasize { 
+		font-weight: light;
+		font-size: smaller;
+		display: block;
+	}
+	.PSFDV-Data { 
+		margin-left: var(--PSFDV-indentation-size);
+		line-height: 0.85;
+		font-size: smaller;
+	}
+	.PSFDV-Label { 
+		font-weight: light;
+		min-width: 15%;
+		display: block;
+		color: var(--PSFDV-Section-button-color);
+		margin: var(--PSFDV-gutter-size) 0;
+	}
+	.PSFDV-Hidden { 
+		display: none;
+	}
+	.PSFDV-Section-Solver-DynamicInput {
+		background-color: #ffffff;
+	}
+	#PSFDV-DynamicInputSection .PSFDV-Section { 
+		margin: var(--PSFDV-gutter-size);
+		padding: var(--PSFDV-gutter-size);
+		background-color: var(--PSFDV-Section-DynamicInput-background-color);
+	}
+
+	#PSFDV-DynamicInputSection .PSFDV-Section .PSFDV-Section-Button { 
+		background-color: var(--PSFDV-Section-DynamicInput-button-color);
+	}
+	#PSFDV-DynamicInputSection .PSFDV-Section .PSFDV-Section-Button a { 
+		color: var(--PSFDV-Section-DynamicInput-button-text-color);
+	}
+	#PSFDV-DynamicInputSection .PSFDV-Section .PSFDV-Label,
+	#PSFDV-DynamicInputSection .PSFDV-Section .PSFDV-DeEmphasize { 
+		color: var(--PSFDV-Section-DynamicInput-button-color);
+	}
+	#PSFDV-DynamicInputSection .PSFDV-Section-Descriptors { 
+		border-left: 5px solid var(--PSFDV-Section-DynamicInput-background-color);
+	}
+	#PSFDV-DynamicInputSection .PSFDV-Section-Group { 
+		border-left: 5px solid var(--PSFDV-Section-DynamicInput-button-color);
+	}
 `,
 
 	Templates: [
 		{
 			Hash: "Pict-Form-DebugViewer-Content",
 			Template: /*html*/`
-<div>
-	<h2>Pict Inline Form Editor</h2>
+<div id="Pict-Form-DebugViewer-Content">
+	<h2 class="PSFDV-Global-Header">Pict Inline Form Tool</h2>
 	{~T:Pict-Form-DebugViewer-MetacontrollerContainer~}
 </div>`
 		},
@@ -50,25 +187,37 @@ const defaultViewConfiguration = (
 			Hash: "Pict-Form-DebugViewer-SectionContainer",
 			Template: /*html*/`
 	<div id="PSFDV-Section-{~D:Record.Hash~}" class="PSFDV-Section">
-		<h3><span class="PSFDV-DeEmphasize">Section:</span> {~D:Record.sectionDefinition.Name~}</h3>
-		<div class="PSDV-Section-Buttons"><a href="#" onclick="{~P~}.views['{~D:Record.View.Hash~}'].render()">[ render ]</a> <a href="#" onclick="{~P~}.ContentAssignment.assignContent('{~D:Record.View.options.DefaultDestinationAddress~}','')">[ clear ]</a> <a href="#" onclick="{~P~}.ContentAssignment.removeClass('#PSFDV-{~D:Record.View.Hash~}-Extra', 'PSFDV-Hidden')">[ Extra Data ]</a></div>
-		<div id="PSFDV-{~D:Record.View.Hash~}-Extra" class="PSFDV-Hidden">
-			<p class="PSFDV-Data"><span class="PSFDV-Label">Description:</span> {~D:Record.sectionDefinition.Description~}</p>
-			<p class="PSFDV-Data"><span class="PSFDV-Label">Hash:</span> {~D:Record.View.Hash~}</p>
-			<p class="PSFDV-Data"><span class="PSFDV-Label">HTML ID:</span> {~D:Record.View.sectionDefinition.Macro.HTMLID~}</p>
-			<h4>Section Solvers:</h4>
+		<h3 class="PSFDV-Section-Header"><span class="PSFDV-DeEmphasize">Section:</span> {~D:Record.sectionDefinition.Name~}</h3>
+		<ul class="PSFDV-Section-Buttons">
+			<li class="PSFDV-Section-Button"><a  href="javascript:void(0);" onclick="{~P~}.views['{~D:Record.View.Hash~}'].render()">Render</a></li>
+			<li class="PSFDV-Section-Button"><a  href="javascript:void(0);" onclick="{~P~}.ContentAssignment.assignContent('{~D:Record.View.options.DefaultDestinationAddress~}','')">Clear</a></li>
+			<li class="PSFDV-Section-Button"><a  href="javascript:void(0);" onclick="{~P~}.ContentAssignment.removeClass('#PSFDV-{~D:Record.View.Hash~}-Extra', 'PSFDV-Hidden')">Extra Data</a></li>
+		</ul>
+		<div id="PSFDV-{~D:Record.View.Hash~}-Extra" class="PSFDV-Hidden PSFDV-Section-ExtraData">
+			<p class="PSFDV-Data">
+				<span class="PSFDV-Label">Description:</span> {~D:Record.sectionDefinition.Description~}
+			</p>
+			<p class="PSFDV-Data">
+				<span class="PSFDV-Label">Hash:</span> {~D:Record.View.Hash~}
+			</p>
+			<p class="PSFDV-Data">
+				<span class="PSFDV-Label">HTML ID:</span> {~D:Record.View.sectionDefinition.Macro.HTMLID~}
+			</p>
+			<h4 class="PSFDV-Content-Header">Section Solvers:</h4>
 			<div class="PSFDV-Section-Solvers">
 				{~TS:Pict-Form-DebugViewer-SolverEntry:Record.Solvers[]<<~?ExpressionScope,==,Section?~>>~}
 			</div>
-			<h4>Tabular/RecordSet Solvers:</h4>
+			<h4 class="PSFDV-Content-Header">Tabular/RecordSet Solvers:</h4>
 			<div class="PSFDV-Group-Solvers">
 				{~TS:Pict-Form-DebugViewer-SolverEntry:Record.Solvers[]<<~?ExpressionScope,==,Group?~>>~}
 			</div>
-			<h4>Inputs:</h4>
+			<h4 class="PSFDV-Content-Header">Inputs:</h4>
 			<div class="PSFDV-Section-Solver-DynamicInput">
 				{~TS:Pict-Form-DebugViewer-GroupContainer:Record.View.sectionDefinition.Groups~}
 			</div>
-			<a href="#" onclick="{~P~}.ContentAssignment.addClass('#PSFDV-{~D:Record.View.Hash~}-Extra', 'PSFDV-Hidden')">[ Hide Extra Data for {~D:Record.View.Hash~} ]</a>
+			<ul class="PSFDV-Section-Buttons">
+				<li class="PSFDV-Section-Button"><a href="javascript:void(0);" onclick="{~P~}.ContentAssignment.addClass('#PSFDV-{~D:Record.View.Hash~}-Extra', 'PSFDV-Hidden')">Hide Extra Data for {~D:Record.View.Hash~}</a></li>
+			</ul>
 		</div>
 	</div>
 `
@@ -87,7 +236,7 @@ const defaultViewConfiguration = (
 			Hash: "Pict-Form-DebugViewer-GroupContainer",
 			Template: /*html*/`
 			<div class="PSFDV-Section-Group">
-				<h8><span class="PSFDV-DeEmphasize">Group:</span> {~D:Record.Name~} [idx <em>{~D:Record.GroupIndex~}</em>]</h8>
+				<h5 class="PSFDV-Content-Header"><span class="PSFDV-DeEmphasize">Group:</span> {~D:Record.Name~} [idx <em>{~D:Record.GroupIndex~}</em>]</h5>
 				<p class="PSFDV-Data"><span class="PSFDV-Label">Layout:</span> {~D:Record.Layout~}</p>
 				<div class="PSFDV-Section-Rows">
 					{~TS:Pict-Form-DebugViewer-RowContainer:Record.Rows~}
@@ -99,7 +248,7 @@ const defaultViewConfiguration = (
 			Hash: "Pict-Form-DebugViewer-RowContainer",
 			Template: /*html*/`
 					<div class="PSFDV-Section-Descriptors">
-						<h9>Row {~D:Record.Hash~}</h9>
+						<h6 class="PSFDV-Content-Header">Row {~D:Record.Hash~}</h6>
 						{~TS:Pict-Form-DebugViewer-DescriptorContainer:Record.Inputs~}
 					</div>
 `
